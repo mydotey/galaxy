d927c4ceb0fdff148ab1e8c81e02a47a79cd2cf9
koqizhao <koqizhao@outlook.com>
2023-02-04 23:04:19 +0800

    1. fix index controller; 2. add more artifacts to bom

---
+++ backend/galaxy-server/galaxy-service/src/main/java/org/mydotey/galaxy/app/rest/controller/IndexController.java
@@ -0,0 +1,22 @@
+package org.mydotey.galaxy.app.rest.controller;
+
+import org.mydotey.galaxy.vo.ViewResponse;
+import org.mydotey.java.StringExtension;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+public class IndexController {
+
+    @RequestMapping(path = "/hello", method = RequestMethod.GET, consumes = MediaType.ALL_VALUE)
+    public ViewResponse<String> hello(@RequestParam(name = "name", required = false) String name) {
+        if (StringExtension.isBlank(name))
+            name = "world";
+        return ViewResponse.success(name);
+    }
+
+}
