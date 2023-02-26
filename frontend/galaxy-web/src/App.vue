35f1cfe2212317d4f63d89d753f057158a86f800
koqizhao <koqizhao@outlook.com>
2023-02-26 15:37:51 +0800

    Simplify code

--- frontend/galaxy-web/src/App.vue
+++ frontend/galaxy-web/src/App.vue
@@ -1,13 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HomeView from './views/HomeView.vue';
 </script>
 
 <template>
   <header>
   </header>
-
-  <HomeView />
 
   <RouterView />
 </template>
